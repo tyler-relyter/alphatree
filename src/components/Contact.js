e05c5a9686d5537930d8c2b71baa85b41e8629af
tyler bullard <hytyler23@gmail.com>
2024-04-16 22:58:37 -0400

    updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,26 @@ const Contact = () => {
     return (
         <div id="contact">
             <div className="container-fluid bg-light">
+                <h2 className="display-2 text-center p-3 m-3">Need A Quote?</h2>
+                <form action="mailto:" method="post" encType="text/plain">
+                    <div className="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" className="form-control" id="name" placeholder="Enter your name" required />
+                    </div>
+                    <div className="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" className="form-control" id="email" placeholder="Enter your email" required />
+                    </div>
+                    <div className="form-group">
+                        <label for="phone">Phone:</label>
+                        <input type="tel" className="form-control" id="phone" placeholder="Enter your phone number" required />
+                    </div>
+                    <div className="form-group">
+                        <label for="message">Message:</label>
+                        <textarea className="form-control" id="message" rows="5" placeholder="Enter your message" required></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
                 <h1 className="display-1 text-center p-3 m-3">Contact Us</h1>
                 <p className="text-center p-3 m-3">Please call us at <strong>555-555-5555</strong> or email us at</p>
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ import treephoto from "../img/treephoto31.jpg";
 import trimmedpine from "../img/trimmed_pine.jpg";
 import nicetree from "../img/nice_trim.jpg";
 import "./css/home.css";
+import bobcat from "../img/bobcat.jpeg";
+import climbpine from "../img/climb_pine.jpg";
+import climbtree from "../img/climb_tree.jpg";
 
 
 const Home = () => {
@@ -33,25 +36,53 @@ const Home = () => {
                 
                 <h6 className="text-center text-uppercase display-4 p-3 m-3">Trust us with your projects</h6>
                 <div className="row">
-                    <h6 className="text-center p-3 m-3 display-6">SERVICES</h6>
+                    <h6 className="text-center p-3 m-3 display-2 bg-dark text-white">SERVICES</h6>
                 </div>
                 <div className="row">
-                    <div className="col">
-                        <h2 className="text-center p-3 m-3">Pruning</h2>
-                        <p className="text-center fs-3 p-3 m-3">Alpha Tree strives to help keep their customers informed on proper pruning practices and its benefits on tree health in the future.</p>
-                    </div>
-                    <div className="col">
-                        <h3 className="text-center p-3 m-3">Removal</h3>
+                    <div className="col border border-success border-3">
+                        <h3 className="display-3 text-center p-3 m-3">Removal</h3>
                         <p className="text-center fs-3 p-3 m-3">Alpha Tree Surgeons is a company that specializes in tree removal. We have the proper tools and equipment to remove trees safely and efficiently, going 
                         above and beyond to keep your property intact. This includes existing landscapes, flowers, grass, or other items.</p>
                     </div>
-                    <div className="col">
-                        <h3 className="text-center p-3 m-3">Consultation Services</h3>
+                    <div className="col border border-success border-3">
+                        <h2 className="display-3 text-center p-3 m-3">Pruning</h2>
+                        <p className="text-center fs-3 p-3 m-3">Alpha Tree strives to help keep their customers informed on proper pruning practices and its benefits on tree health in the future.</p>
+                    </div>
+                    <div className="col border border-success border-3">
+                        <h3 className="display-3 text-center p-3 m-3">Consultation Services</h3>
                         <p className="text-center fs-3 p-3 m-3">Chat with a pro at Alpha Tree to determine the health of the trees on your property. We provide a detailed plan on what should be done to keep the trees on your property
                         safe and disease-free. <a href="#contact">Call us</a> today and we'll send one of our experts to walk through and show you spots of decay, underlying illnesses, issues with bark or cambium layer, pests, etc.</p>
                     </div>
                 </div>
-                
+                <div className="row">
+                    <div className="col"></div>
+                    <div className="col">
+                        <h3 className="display-3 text-center p-3 m-3">Our Work in Action</h3>
+                        <div id="carouselTree" class="carousel slide">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                <img src={climbtree} class="img-fluid d-block w-100" style={{ height: '650px', width: '500px'}} alt="..."/>
+                                </div>
+                                <div class="carousel-item">
+                                <img src={bobcat} class="img-fluid d-block w-100" style={{ height: '650px', width: '500px'}} alt="..."/>
+                                </div>
+                                <div class="carousel-item">
+                                <img src={climbpine} class="img-fluid d-block w-100" style={{ height: '650px', width: '500px'}} alt="..."/>
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselTree" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselTree" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col"></div>
+
+                </div>
             </div>
             <About />
             <Contact />
