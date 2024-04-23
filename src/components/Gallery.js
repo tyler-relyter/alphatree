2611e5e6008261076d8dae44eb7ce4f7af085b9e
tyler bullard <hytyler23@gmail.com>
2024-04-22 22:19:59 -0400

    fullscreen and header

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 import sideyard from "../img/sideyard.jpg";
 import sideyard_b4 from "../img/sideyard_b4.jpg";
 import sideyard_after from "../img/sideyard_after.jpg";
@@ -11,56 +12,77 @@ import chicken_bobcat from "../img/chicken_bobcat.jpg";
 import chicken_lawnmower from "../img/chicken_lawnmower.jpg";
 import firewood from "../img/firewood.jpg";
 import saw_on_stump2 from "../img/saw_on_stump2.jpg";
+import "./css/gallery.css";
 
 
 const Gallery = () => {
+    const [show, setShow] = useState(false);
+    const [currentImage, setCurrentImage] = useState('');
+
+    const handleClose = () => setShow(false);
+    const handleShow = (imgSrc) => {
+        setCurrentImage(imgSrc);
+        setShow(true);
+    }
+    
+
     return (
         <div id="gallery">
             <div className="container-fluid">
                 <h1 className="display-1 text-center p-3 m-3"><b>Gallery</b></h1>
                 <div className="row">
                     <div className="col">
-                        <img src={sideyard_b4} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={sideyard_b4} onClick={() => handleShow(sideyard_b4)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px', cursor: 'pointer'}} alt="Tree Surgeons" />
                     </div>
                     <div className="col">
-                        <img src={sideyard} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={sideyard} onClick={() => handleShow(sideyard)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                     <div className="col">
-                        <img src={sideyard_after} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={sideyard_after} onClick={() => handleShow(sideyard_after)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                     <div className="col">
-                        <img src={backlot} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <img src={backlot_done} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
-                    </div>
-                    <div className="col">
-                        <img src={blue_building} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
-                    </div>
-                    <div className="col">
-                        <img src={beside_driveway} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
-                    </div>
-                    <div className="col">
-                        <img src={beside_driveway2} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={backlot} onClick={() => handleShow(backlot)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                 </div>
                 <div className="row">
                     <div className="col">
-                        <img src={chicken_bobcat} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={backlot_done} onClick={() => handleShow(backlot_done)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                     <div className="col">
-                        <img src={firewood} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={blue_building} onClick={() => handleShow(blue_building)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                     <div className="col">
-                        <img src={chicken_lawnmower} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={beside_driveway} onClick={() => handleShow(beside_driveway)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                     <div className="col">
-                        <img src={saw_on_stump2} className="img-fluid m-5 p-3" style={{ height: '500px', width: '450px'}} alt="Tree Surgeons" />
+                        <img src={beside_driveway2} onClick={() => handleShow(beside_driveway2)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <img src={chicken_bobcat} onClick={() => handleShow(chicken_bobcat)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
+                    </div>
+                    <div className="col">
+                        <img src={firewood} onClick={() => handleShow(firewood)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
+                    </div>
+                    <div className="col">
+                        <img src={chicken_lawnmower} onClick={() => handleShow(chicken_lawnmower)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
+                    </div>
+                    <div className="col">
+                        <img src={saw_on_stump2} onClick={() => handleShow(saw_on_stump2)} className="img-fluid m-5 p-3" id="imgThumbnail" style={{ height: '450px', width: '450px'}} alt="Tree Surgeons" />
                     </div>
                 </div>
             </div>
+
+            <Modal show={show} onHide={handleClose} className="customModal">
+                <Modal.Header closeButton>
+                    <Modal.Title>Image</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img src={currentImage} className="img-fluid" alt="Tree Surgeons" />
+                </Modal.Body>
+            </Modal>
+
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ const Navbar = () => {
     
     return (
         
-            <nav id="nav" className="navbar navbar-expand-lg navbar-light sticky-top">
+            <nav id="nav" className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
                     <div className="dropdown justify-content-start">
-                        <button className="btn btn-lg btn-white border border-white text-white dropdown dropdown-toggle m-3 p-3" type="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <button className="btn btn-white border border-white text-white dropdown dropdown-toggle m-3 p-2" type="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Menu</button>
                         <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
                             <a className="dropdown-item" href='/'>Home</a>
@@ -21,7 +21,7 @@ const Navbar = () => {
                     </div>
                     <div className="">
                         
-                        <h1 className="display-1 text-white text-uppercase m-2"><i className="fas fa-tree text-white m-2"></i>Alpha Tree Surgeons<i className="fas fa-tree text-white m-2"></i></h1>
+                        <h1 className="display-5 text-white text-uppercase m-2"><i className="fas fa-tree text-white m-2"></i>Alpha Tree Surgeons</h1>
                         <div className="d-flex justify-content-end">
                             <p className="text-white fs-2 mx-5"><a href="tel:+19105208878"><strong>+1-910-520-8878</strong></a></p>
                             <i className="fab fa-facebook-square fa-3x text-white m-2"></i>
