6c7d7d641928b8a5cf47d1c0e94614e62840d840
tyler bullard <hytyler23@gmail.com>
2024-05-08 11:45:54 -0400

    link tweaking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-
+import "./css/nav.css";
 
 
 
@@ -15,17 +14,17 @@ const Navbar = () => {
                             Menu</button>
                         <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
                             <a className="dropdown-item" href='/'>Home</a>
-                            <a className="dropdown-item" href='#about'>About</a>
-                            <a className="dropdown-item" href='#contact'>Contact Us</a>
-                            <a className="dropdown-item" href='#gallery'>Gallery</a>
+                            <a className="dropdown-item" href='/#about'>About</a>
+                            <a className="dropdown-item" href='/#contact'>Contact Us</a>
+                            <a className="dropdown-item" href='/#gallery'>Gallery</a>
                         </div>
                     </div>
                     <div className="">
                         
-                        <h1 className="display-5 text-white text-uppercase m-2"><i className="fas fa-tree text-white m-2"></i>Alpha Tree Surgeons</h1>
+                        <h1 className="display-5 text-white text-uppercase m-2"><i className="fas fa-tree text-white m-2"></i><strong>Alpha Tree Surgeons</strong></h1>
                         <div className="d-flex justify-content-end">
-                            <p className="text-white fs-2 mx-5"><a href="tel:+19105208878"><strong>+1-910-520-8878</strong></a></p>
-                            <a href="https://www.facebook.com/AlphaTreeSurgeons?mibextid=ZbWKwL"><i className="fab fa-facebook-square fa-3x text-white m-2"></i></a>
+                            <p className="fs-2 mx-5"><a id="phone-link" href="tel:+19105208878"><strong>+1-910-520-8878</strong></a></p>
+                            <a href="https://www.facebook.com/AlphaTreeSurgeons?mibextid=ZbWKwL"><i id="fb-link" className="fab fa-facebook-square fa-3x m-2"></i></a>
                             
                         </div>
                     </div>
