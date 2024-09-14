4749d2a867936fe5ac8a9b8b9b5936aeb75eb6e9
tyler bullard <hytyler23@gmail.com>
2024-09-14 11:13:25 -0400

    ch

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/alphatree/" element={<Home />} />
+        <Route path="/alphatree/about" element={<About />} />
+        <Route path="/alphatree/contact" element={<Contact />} />
       </Routes>
     </div>
     </Router>
